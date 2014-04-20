fdd7cc4d905db50681b6495479b2ecd1a1349621
Brannon Dorsey <brannon@brannondorsey.com>
2014-04-20 15:58:49 -0500

    Using localstorage to keep minimum wage. Now works with https amazon as well

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,43 +1,66 @@
 var hourlyIncome;
-var defaultHourlyIncome = 7.25;
 var priceRegex = /\$[\d]+[\.]*[\d,]*/ig;
 var refreshRate = 2; // in seconds
 var priceContainers = [
 	".price",
+	".listprice",
 	"span",
+	"td",
+	"del",
 	"b", 
 	"strong", 
 	"strike"
 ];
 
 $(document).ready(function(){
+	console.log("has access");
+	getHourlyIncome(function(_hourlyIncome){
+		hourlyIncome = _hourlyIncome;
+		replacePrices();
+		setInterval(function(){
+			if (document.hasFocus) {
+				replacePrices();
+			}
+		}, refreshRate * 1000);
+	});
 
-
-    $.ajax({
-        url: chrome.extension.getURL("data/wages.json"),
-        success: function(data){
-        	var states = JSON.parse(data);
-        	
-        	$.ajax({
-				url: "http://www.telize.com/ip",
-				success: function(data) {
-
-					$.getJSON("https://freegeoip.net/json/" + data.trim(), function(data){
-			
-						hourlyIncome = states[data.region_code] || defaultHourlyIncome;
-
-						replacePrices();
-						setInterval(function(){
-							if (document.hasFocus) {
-								replacePrices();
-							}
-						}, refreshRate * 1000);
-					});
-				}
-			});
-        }
-    });
 });
+
+function getHourlyIncome(callback) {
+	
+	var hourlyIncome = 7.25; // default
+	
+	if (localStorage) {
+		var storedHourlyIncome = localStorage.getItem("stateMinimumWage");
+		if (storedHourlyIncome) {
+			callback(storedHourlyIncome);
+		} else {
+			console.log("localStorage not found making requests");
+			$.ajax({
+		        url: chrome.extension.getURL("data/wages.json"),
+		        success: function(data){
+		        	var states = JSON.parse(data);
+		        	
+		        	$.ajax({
+						url: "http://www.telize.com/ip",
+						success: function(data) {
+
+							$.getJSON("https://freegeoip.net/json/" + data.trim(), function(data){
+					
+								if (states[data.region_code]) hourlyIncome = states[data.region_code];
+								localStorage.setItem("stateMinimumWage", hourlyIncome);
+								callback(hourlyIncome);
+							});
+						}
+					});
+		        }
+		    });
+		}
+	} else callback(hourlyIncome);
+
+	
+}
+
 
 function replacePrices() {
 	
@@ -67,5 +90,3 @@ function replacePrices() {
 function capitalize(s) {
     return s[0].toUpperCase() + s.slice(1);
 }
-
-
